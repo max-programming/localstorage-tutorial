c7839421b404ba1aec10d42b213b3d856d73b5b4
Max Programming <51731966+max-programming@users.noreply.github.com>
2021-04-19 13:56:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
+// DOM Elements
 const studentForm = document.getElementById("studentForm");
-const studentsDiv = document.querySelector(".students");
+const studentsContainer = document.querySelector(".students");
 const nameInput = studentForm["name"];
 const ageInput = studentForm["age"];
 const rollInput = studentForm["roll"];
 
+/* 
+{
+  name: '',
+  age: number,
+  roll: number
+}
+*/
+
 const students = JSON.parse(localStorage.getItem("students")) || [];
 
 const addStudent = (name, age, roll) => {
-  students.push({ name, age, roll });
+  students.push({
+    name,
+    age,
+    roll,
+  });
+
   localStorage.setItem("students", JSON.stringify(students));
+
   return { name, age, roll };
 };
 
 const createStudentElement = ({ name, age, roll }) => {
+  // Create elements
   const studentDiv = document.createElement("div");
   const studentName = document.createElement("h2");
   const studentAge = document.createElement("p");
   const studentRoll = document.createElement("p");
 
-  studentName.innerText = `Student name: ${name}`;
-  studentAge.innerText = `Student age: ${age}`;
-  studentRoll.innerText = `Student roll: ${roll}`;
+  // Fill the content
+  studentName.innerText = "Student name: " + name;
+  studentAge.innerText = "Student age: " + age;
+  studentRoll.innerText = "Student roll: " + roll;
 
+  // Add to the DOM
   studentDiv.append(studentName, studentAge, studentRoll);
-  studentsDiv.appendChild(studentDiv);
+  studentsContainer.appendChild(studentDiv);
 
-  // @ts-ignore
-  studentsDiv.style.display = students.length === 0 ? "none" : "flex";
+  studentsContainer.style.display = students.length === 0 ? "none" : "flex";
 };
 
-// @ts-ignore
-studentsDiv.style.display = students.length === 0 ? "none" : "flex";
+studentsContainer.style.display = students.length === 0 ? "none" : "flex";
 
 students.forEach(createStudentElement);
 

9cf526781d56644e8c49a8d7460d539bd00b8a1c
Usman <ui.sabuwala@gmail.com>
2022-07-17 13:01:28 +0530

    Added edit button

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // DOM Elements
-const studentForm = document.getElementById("studentForm");
-const studentsContainer = document.querySelector(".students");
-const nameInput = studentForm["name"];
-const ageInput = studentForm["age"];
-const rollInput = studentForm["roll"];
+const studentForm = document.getElementById('studentForm');
+const studentsContainer = document.querySelector('.students');
+const nameInput = studentForm['name'];
+const ageInput = studentForm['age'];
+const rollInput = studentForm['roll'];
+
+const EDIT_ICON = `<svg class="btn-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>`;
 
 /* 
 {
@@ -13,7 +15,7 @@ const rollInput = studentForm["roll"];
 }
 */
 
-const students = JSON.parse(localStorage.getItem("students")) || [];
+const students = JSON.parse(localStorage.getItem('students')) || [];
 
 const addStudent = (name, age, roll) => {
   students.push({
@@ -22,31 +24,37 @@ const addStudent = (name, age, roll) => {
     roll,
   });
 
-  localStorage.setItem("students", JSON.stringify(students));
+  localStorage.setItem('students', JSON.stringify(students));
 
   return { name, age, roll };
 };
 
 const createStudentElement = ({ name, age, roll }) => {
   // Create elements
-  const studentDiv = document.createElement("div");
-  const studentName = document.createElement("h2");
-  const studentAge = document.createElement("p");
-  const studentRoll = document.createElement("p");
+  const studentDiv = document.createElement('div');
+  const studentName = document.createElement('h2');
+  const studentAge = document.createElement('p');
+  const studentRoll = document.createElement('p');
+  const btnsDiv = document.createElement('div');
+  const editButton = document.createElement('button');
 
+  btnsDiv.className = 'btns-div';
+  editButton.className = 'edit-btn';
   // Fill the content
-  studentName.innerText = "Student name: " + name;
-  studentAge.innerText = "Student age: " + age;
-  studentRoll.innerText = "Student roll: " + roll;
+  studentName.innerText = 'Student name: ' + name;
+  studentAge.innerText = 'Student age: ' + age;
+  studentRoll.innerText = 'Student roll: ' + roll;
+  editButton.innerHTML = EDIT_ICON;
 
   // Add to the DOM
-  studentDiv.append(studentName, studentAge, studentRoll);
+  btnsDiv.append(editButton);
+  studentDiv.append(studentName, studentAge, studentRoll, btnsDiv);
   studentsContainer.appendChild(studentDiv);
 
-  studentsContainer.style.display = students.length === 0 ? "none" : "flex";
+  studentsContainer.style.display = students.length === 0 ? 'none' : 'flex';
 };
 
-studentsContainer.style.display = students.length === 0 ? "none" : "flex";
+studentsContainer.style.display = students.length === 0 ? 'none' : 'flex';
 
 students.forEach(createStudentElement);
 
@@ -61,7 +69,7 @@ studentForm.onsubmit = e => {
 
   createStudentElement(newStudent);
 
-  nameInput.value = "";
-  ageInput.value = "";
-  rollInput.value = "";
+  nameInput.value = '';
+  ageInput.value = '';
+  rollInput.value = '';
 };
